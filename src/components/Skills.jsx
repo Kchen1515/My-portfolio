66905b6798400efe16272bba5d28038ad97a918d
Kong Yu Chen <kongyuchen@Kongs-Laptop.fios-router.home>
2023-01-01 18:38:56 -0500

    update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import Skill from './Skill.jsx'
 
 const Skills = () => {
   return (
-    <section id="skills" className={`${styles.flexCenter} flex-col w-fullx `}>
+    <section id="skills" className={`${styles.flexCenter} flex-col w-full `}>
       <div className="mb-5">
         <h3 className="font-semibold text-3xl">Skills</h3>
       </div>
@@ -35,12 +35,12 @@ const Skills = () => {
       </div>
       <div class="relative flex overflow-x-hidden w-3/4">
         <div class="py-8 whitespace-nowrap animate-marquee">
-          {skills.slice(12,19).map((skill, i) => {
+          {skills.slice(12,18).map((skill, i) => {
             return <img src={skill.imgSrc} key={skill.id} className="w-24 mx-5"/>
           })}
         </div>
         <div class="absolute top-0 py-8 animate-marquee2 whitespace-nowrap">
-          {skills.slice(12,19).map((skill, i) => {
+          {skills.slice(12,18).map((skill, i) => {
             return <img src={skill.imgSrc} key={skill.id} className="w-24 mx-5"/>
           })}
         </div>
