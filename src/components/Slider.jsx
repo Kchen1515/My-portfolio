9f3f8bde70a86bc12e39d809c576473361eb464c
Kchen1515 <kychen249@gmail.com>
2023-04-17 22:00:30 -0400

    adjust marquee styling

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,21 +5,30 @@ import { skills } from "../constants";
 
 const Slider = () => {
   return (
-    <div className="w-1/2 flex flex-col gap-10 m-10">
-      <Marquee speed="75">
+    <div className="w-full flex flex-col gap-10 m-10">
+      <Marquee
+        speed="75"
+        pauseOnHover="true"
+        gradientWidth={100}
+      >
         {
           skills.slice(0,9).map((skill, i) => {
             return (
-              <img src={skill.imgSrc} alt="logo of technology" className="w-[100px]  mr-[75px]"/>
+              <img src={skill.imgSrc} alt="logo of technology" className="w-[50px]  mr-[75px] sm:w-[100px]"/>
             )
           })
         }
       </Marquee>
-      <Marquee speed="75">
+      <Marquee
+        speed="75"
+        pauseOnHover="true"
+        direction='right'
+        gradientWidth={100}
+      >
         {
           skills.slice(9,18).map((skill, i) => {
             return (
-              <img src={skill.imgSrc} alt="logo of technology" className="w-[100px] mr-[75px]"/>
+              <img src={skill.imgSrc} alt="logo of technology" className=" w-[50px] mr-[75px] sm:w-[100px]"/>
             )
           })
         }
